441d1a4a1de028dfb8e82748fc533933489f1dec
helibertoarias <HelibertoArias@gmail.com>
2019-10-30 20:24:22 -0500

    feat: implement custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
+import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpaces],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
